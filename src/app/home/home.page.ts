2534ed52cd2dc1cac2db4ff32fa2129c23d5453b
Surraj Ramanathan <tp050803@mail.apu.edu.my>
2019-08-29 11:58:38 +0800

    new test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { HttpClient,HttpHeaders} from '@angular/common/http'
+import { map } from 'rxjs/operators'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username = "TP050803@mail.apu.edu.my"
+  password = "surraj34"
+  searchTerm
+  data;
+  constructor(private alertCtrl: AlertController, private http: HttpClient) {}
 
-  constructor() {}
 
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  getData(){
+    const url = `https://api.bitbucket.org/2.0/repositories/ExplodingCow_apu/${this.searchTerm}/commits/`
+    console.log(this.searchTerm)
+    let Httpheaders = new HttpHeaders()
+    Httpheaders.append('username', this.username);
+    Httpheaders.append('password', this.password);
+    this.http.get(url,{headers:Httpheaders}).subscribe(resp=>{
+      this.data = resp["values"]
+      console.log(this.data)
+    })
+  }
 }
